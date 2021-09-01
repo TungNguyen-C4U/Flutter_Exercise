b2f4bb77ea4bbc35075b1613f3dc7b39f99f6641
Tung Nguyen <nlxuantung@gmail.com>
2021-09-01 14:16:56 +0700

    Change FlatButton to TextButton

--- flutter_demo_quiz/lib/result.dart
+++ flutter_demo_quiz/lib/result.dart
@@ -32,10 +32,39 @@ class Result extends StatelessWidget {
           ),
           FlatButton(
             child: Text(
-              'Restart Quiz!',
+              'Restart Quiz! - FlatButton has been deprecated',
             ),
             textColor: Colors.blue,
-            onPressed: resetHandler,
+            onPressed: null,
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(4),
+            child: Stack(
+              children: <Widget>[
+                Positioned.fill(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: <Color>[
+                          Color(0xFF0D47A1),
+                          Color(0xFF1976D2),
+                          Color(0xFF42A5F5),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.all(16.0),
+                    primary: Colors.white,
+                    textStyle: const TextStyle(fontSize: 20),
+                  ),
+                  onPressed: resetHandler,
+                  child: const Text('Restart Quiz!'),
+                ),
+              ],
+            ),
           ),
         ],
       ),
